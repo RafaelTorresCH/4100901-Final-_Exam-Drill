bd50471fb712e72c18cf280e763061e0336de00d
RafaelTorresCH <rtorresch@unal.edu.co>
2024-09-09 18:50:31 -0500

    add_keypad.h,  keypad.c and heardbeat

---
+++ Core/Inc/KEYPAD_r.h
@@ -0,0 +1,13 @@
+/*
+ * KEYPAD_r.h
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: torre
+ */
+
+#ifndef INC_KEYPAD_R_H_
+#define INC_KEYPAD_R_H_
+
+
+
+#endif /* INC_KEYPAD_R_H_ */
---
+++ Core/Src/KEYPAD_R.c
@@ -0,0 +1,8 @@
+/*
+ * KEYPAD_R.c
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: torre
+ */
+
+
